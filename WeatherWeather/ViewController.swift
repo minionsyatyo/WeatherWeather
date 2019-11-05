21dbf5f797555486bf8969ac5b535eabd46f1ee5
Oga Yoshihara <ouga1120ouga@gmail.com>
2019-11-05 20:07:51 +0900

    Initial Commit

---
+++ WeatherWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherWeather
+//
+//  Created by 吉原央賀 on 2019/11/05.
+//  Copyright © 2019 ouga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
